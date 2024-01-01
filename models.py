b8d676edcfbc0fa8b07c57656ea3784d07e3ae14
leoBitto <leonardobitto@gmail.com>
2024-01-01 17:03:33 +0100

    modified models

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Booking(models.Model):
+
     name = models.CharField(max_length=100, blank=True, null=True)
     date = models.DateField()
     hour = models.TimeField(blank=True, null=True)
     num_of_people = models.IntegerField(blank=True, null=True)    
-    phone_number = models.CharField(max_length=10, blank=True, null=True)  # Cambiato da IntegerField a CharField
+    phone_number = models.CharField(max_length=10, blank=True, null=True)  
     note = models.TextField(blank=True, null=True)
 
     def __str__(self):
